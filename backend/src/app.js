7358493869d8594831f3c21e516c2e8ae0a5da7a
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-05 21:43:09 +0530

    foot route

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes')
+const foodRoutes = require('./routes/food.routes')
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.get("/",(req,res)=>{
     res.send("hello world");
 })
 app.use('/api/auth', authRoutes)
+app.use('/api/food', foodRoutes)
 
 module.exports = app;
---
+++ backend/src/routes/food.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router =express.Router();
+
+/* POST /api/food/ [protected] */
+router.post('/')
+
+module.exports = router;
