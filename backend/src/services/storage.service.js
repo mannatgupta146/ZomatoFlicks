ffbe5dedb7fc5dc1fffea56f17b2949dae0e650d
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-06 22:51:01 +0530

    image kit

---
+++ backend/src/services/storage.service.js
@@ -0,0 +1,7 @@
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+    publicKey : "your_public_api_key",
+    privateKey : "your_private_api_key",
+    urlEndpoint : "https://ik.imagekit.io/your_imagekit_id/"
+});
