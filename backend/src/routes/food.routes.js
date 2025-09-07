c98126c9e7296f73da6b3e66945bafa4fa44706f
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-07 18:50:03 +0530

    backend

--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -2,15 +2,15 @@ const express = require('express')
 const foodController = require('../controllers/food.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const multer = require('multer')
-const router =express.Router();
+const router = express.Router();
 
 const upload = multer({
     storage: multer.memoryStorage(),
 })
 
 /* POST /api/food/ [protected] */
-router.post('/',authMiddleware.authFoodPartnerMiddleware, upload.single("video"), foodController.createFood)
+router.post('/', authMiddleware.authFoodPartnerMiddleware, upload.single("video"), foodController.createFood)
 
-router.get('/', )
+router.get('/', authMiddleware.authUserMiddleware, foodController.getFoodItems)
 
 module.exports = router; 
