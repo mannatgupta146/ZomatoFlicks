8d13c5435d2f8221a88e2de684bed5a1ac323711
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-08 20:05:32 +0530

    home created

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import ChooseRegister from '../pages/auth/ChooseRegister';
 import UserLogin from '../pages/auth/UserLogin';
 import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister';
 import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin';
+import Home from '../pages/general/Home'
 
 const AppRoutes = () => {
   return (
@@ -15,6 +16,7 @@ const AppRoutes = () => {
               <Route path="/user/login" element={<UserLogin />} />
               <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
               <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
+              <Route path="/" elemen={<Home/>}/>
         </Routes>
     </Router>
   )
