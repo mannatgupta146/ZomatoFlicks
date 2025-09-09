d17b58d26493e050d5c79d8a12b5489d2d4ff4cb
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-09 22:53:04 +0530

    save fixed

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,6 +1,7 @@
 const foodModel = require('../models/food.model')
 const storageService = require('../services/storage.service')
 const likeModel = require('../models/likes.model')
+const saveModel = require('../models/save.model')
 const { v4: uuid} = require('uuid')
 
 async function createFood(req, res){
@@ -67,8 +68,70 @@ async function likeFood(req, res){
     })
 }
 
+
+async function saveFood(req, res) {
+
+    const { foodId } = req.body;
+    const user = req.user;
+
+    const isAlreadySaved = await saveModel.findOne({
+        user: user._id,
+        food: foodId
+    })
+
+    if (isAlreadySaved) {
+        await saveModel.deleteOne({
+            user: user._id,
+            food: foodId
+        })
+
+        await foodModel.findByIdAndUpdate(foodId, {
+            $inc: { savesCount: -1 }
+        })
+
+        return res.status(200).json({
+            message: "Food unsaved successfully"
+        })
+    }
+
+    const save = await saveModel.create({
+        user: user._id,
+        food: foodId
+    })
+
+    await foodModel.findByIdAndUpdate(foodId, {
+        $inc: { savesCount: 1 }
+    })
+
+    res.status(201).json({
+        message: "Food saved successfully",
+        save
+    })
+
+}
+
+async function getSaveFood(req, res) {
+
+    const user = req.user;
+
+    const savedFoods = await saveModel.find({ user: user._id }).populate('food');
+
+    if (!savedFoods || savedFoods.length === 0) {
+        return res.status(404).json({ message: "No saved foods found" });
+    }
+
+    res.status(200).json({
+        message: "Saved foods retrieved successfully",
+        savedFoods
+    });
+
+}
+
+
 module.exports = {
     createFood,
     getFoodItems,
-    likeFood
+    likeFood,
+    saveFood,
+    getSaveFood
 }
---
+++ backend/src/models/save.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const saveSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    }
+},{
+timestamps: true
+})
+
+const saveModel = mongoose.model('save', saveSchema)
+
+module.exports = saveModel
