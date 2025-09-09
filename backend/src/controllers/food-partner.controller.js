abd7c4cd200dd2bed4211ba71b0c2c915878149b
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-09 22:09:02 +0530

    completed

--- backend/src/controllers/food-partner.controller.js
+++ backend/src/controllers/food-partner.controller.js
@@ -1,9 +1,13 @@
 const foodPartnerModel = require('../models/foodPartner.model')
+const foodModel = require('../models/food.model')
 
 async function getFoodPartnerById(req, res){
     const foodPartnerId = req.params.foodPartnerId
 
     const foodPartner = await foodPartnerModel.findById(foodPartnerId)
+    const foodItemsByFoodPartner = await foodModel.find({
+        foodPartner: foodPartnerId
+    })
 
     if(!foodPartner){
         return res.status(404).json({
@@ -13,7 +17,10 @@ async function getFoodPartnerById(req, res){
 
     res.status(200).json({
         message: "Food partner retrieved successfully",
-        foodPartner
+        foodPartner: {
+            ...foodPartner.toObject(),
+            foodItems: foodItemsByFoodPartner
+        }
     })
 }
 
