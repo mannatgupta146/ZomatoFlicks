3085cde975e5cf35ee21e83560b3eaab57ea19a4
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-10 00:12:15 +0530

    frontend done

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -10,10 +10,12 @@ router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
 
 
+
 // food partner auth APIs
 router.post('/food-partner/register', authController.registerFoodPartner)
 router.post('/food-partner/login', authController.loginFoodPartner)
 router.get('/food-partner/logout', authController.logoutFoodPartner)
+
 
 
 module.exports = router;
